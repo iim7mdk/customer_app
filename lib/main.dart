3806c29e677283dc588f7451864e2589607f759a
Mohammed <m7md.k2003@gmail.com>
2026-05-18 00:47:25 +0300

    New version

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:customer_app/product_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // ملف Firebase الذي تم إنشاؤه
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,14 +26,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // locale: Locale('ar'), // لتفعيل اللغة العربية
-      // supportedLocales: [Locale('ar')],
-      localizationsDelegates: [
-        DefaultMaterialLocalizations.delegate,
-        DefaultWidgetsLocalizations.delegate,
-      ],
+
       home: Directionality(
-        textDirection: TextDirection.rtl, // كل المحتوى من اليمين إلى اليسار
+        textDirection: TextDirection.rtl,
         child: ProductListScreen(),
       ),
     );
